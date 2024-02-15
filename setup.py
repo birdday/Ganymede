8413932047f76734037472ed6afc6a09a3a2382f
Brian Day <22bday@gmail.com>
2024-02-14 20:06:16 -0500

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='lcparser',
+      version='0.0.1',
+      description='Liquid Chromatography data parser for Ganymede interview.',
+      packages=['lcparser'],
+      install_requires=[
+          'numpy',
+          'scipy'
+          'matplotlib',
+          'mpl_interactions',
+          'BaselineRemoval',
+          'ipympl'
+          'ipykernel'
+          'pytest'
+      ]
+)
